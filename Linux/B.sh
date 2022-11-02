6c9932f74876965d6fc53c0bd8416b3c7be90dcf
Kaseki1 <kaseki1@protonmail.com>
2022-11-02 18:21:30 +0200

    Completed task B in linux tasks

--- Linux/B.sh
+++ Linux/B.sh
@@ -6,3 +6,19 @@ echo -n 'The most requested page:'
 cat apache_logs.txt | cut -d' ' -f7 | sort | uniq -c | sort -nr | head -1
 echo 'Number of requests from each IP:'
 cat apache_logs.txt | cut -d' ' -f1 | sort | uniq -c | sort -n
+echo 'Non-existent pages were clients referred to:'
+buf=$(cat apache_logs.txt)
+while IFS= read -r line
+do
+    if [[ $(cut -d' ' -f9 <<< "$line") == "404" ]]
+    then
+        echo $(cut -d' ' -f8 <<< "$line")
+    fi
+done < apache_logs.txt
+echo -n 'Site got most requests on '
+cat apache_logs.txt | cut -d' ' -f4 | cut -d'/' -f3 | sort | uniq -c | sort -nr | head -1 | cut -d':' -f2,3,4
+echo 'Search bots accessed the site:'
+while IFS= read -r line;
+do
+    echo $(cut -d' ' -f12 <<< "$line") $(cut -d' ' -f1 <<< "$line")
+done < apache_logs.txt
